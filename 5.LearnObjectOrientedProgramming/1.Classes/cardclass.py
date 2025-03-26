88ab632189d13d794e21d9d725647ec214162e7b
okannnn <34797090+okannnn@users.noreply.github.com>
2025-03-26 08:38:52 +0000

    Notes added to cardclass.py
    Added notes/research regarding cardclass.py

--- 5.LearnObjectOrientedProgramming/1.Classes/cardclass.py
+++ 5.LearnObjectOrientedProgramming/1.Classes/cardclass.py
@@ -12,13 +12,27 @@
     A cards' properties will be stored in a tuple. 
     This includes suit, rank. Value should be included for calculations but need not be presented to a user.
 
-    (str(Rank), str(Suit), int(Value)) #How do i encode Aces
+    (str(Rank), str(Suit), int(Value)) #How do i encode Aces?
 
     other properties I've not considered yet.
 
+    #issues
+    I had deckOfCards defined as a global variable. When more than one instance of a deck was instantiated, functions applied to one object affected all other objects. 
+        Resolved by creating __init__ function. Unsure if the __init__ is needed for this, but is also used to define a seed for deterministic shuffling of cards.
+
+        "Static variables are shared by every instance of the class."
+        "Instance variables are defined only within methods, so __init__ or any other class function is required to make a object dependent variable."
+
+    random.Random() is a local random number generator which produces the same values given the input. By doing this, it creates a deterministic randomness to be used consistently.
+    This is likely needed to be moved elsewhere where needed, but for now functions as a seeded randomiser for a shuffle.
+    random.shuffle() is used to return a reordered list, which is now determined by random.Random(seed).
+
+    drawCard function removes a card from the deck and returns the card tuple.
+        There isn't a method to restore the deck to original decksize. It may be that a reference copy of the cardset may be needed to restore from so that cards can be reintroduced into the deck.
+
 """
 import random
-
+#Random used to shuffle a new instance of a deck.
 
 class deckOfCards:
     
@@ -89,9 +103,6 @@ class deckOfCards:
         return len(self.cardset)
 
 """
-    def shuffle(self):
-        random.shuffle(self.cardset)
-"""
 x = deckOfCards(1)
 
 y = deckOfCards(1)
@@ -105,5 +116,5 @@ c2 = y.drawCard()
 print(c1)
 print(c2)
 print(c1 == c2)
-
+"""
 
